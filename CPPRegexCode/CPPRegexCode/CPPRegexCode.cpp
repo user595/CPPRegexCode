91bce6b87ec0bad3b43d126b78af2a2dba599eab
user595 <dwightwilkins@gmail.com>
2014-02-13 23:43:28 -0600

    first, last  parameters
    added code to test the first, last parameters.

--- CPPRegexCode/CPPRegexCode/CPPRegexCode.cpp
+++ CPPRegexCode/CPPRegexCode/CPPRegexCode.cpp
@@ -15,9 +15,48 @@ int _tmain(int argc, _TCHAR* argv[])
     std::regex rx("abc"); 
     std::regex_constants::match_flag_type fl = std::regex_constants::match_default; 
 
+	std::cout << std::endl << "first:" << first << " rx:" <<  "abc   fl:" << fl << std::endl;
+
 	std::cout << "search(f, f+1, \"abc\") == " << std::boolalpha 
         << regex_search(first, first + 1, rx, fl) << std::endl; 
  
+	std::cout << std::endl << "end of regex_search first search" << std::endl;
+
+	std::cout << std::endl << "first:" << first << " *first + 1:" << *first + 1 << " " << "" << char(*first + 1) << std::endl;
+
+	std::cout << "test 10   search(f, f+1, \"abc\") == " << std::boolalpha 
+        << 
+		/*regex_search(test10, test10+ 1, rx, fl)*/
+		regex_search("abxcabc", rx, fl)
+		<< std::endl; 
+	std::cout << "\nend of test10 \n";
+
+	std::cout << "test 11:size is + 2 == " << std::boolalpha 
+        << 
+		/*regex_search(test10, test10+ 1, rx, fl)*/
+		regex_search(first,first + 2, rx, fl)
+		<< std::endl; 
+
+	std::cout << "test 11:size is + 3 == " << std::boolalpha 
+        << 
+		/*regex_search(test10, test10+ 1, rx, fl)*/
+		regex_search(first,first + 3, rx, fl)
+		<< std::endl; 
+
+	std::cout << "test 11:size is + 4 == " << std::boolalpha 
+        << 
+		/*regex_search(test10, test10+ 1, rx, fl)*/
+		regex_search(first,first + 4, rx, fl)
+		<< std::endl; 
+
+	std::cout << "test 11:size is + 14 == " << std::boolalpha 
+        << 
+		/*regex_search(test10, test10+ 1, rx, fl)*/
+		regex_search(first, first + 14, rx, fl)
+		<< std::endl; 
+
+	std::cout << "\nend of test11 \n";
+
     std::cout << "search(f, l, \"abc\") == " << std::boolalpha 
         << regex_search(first, last, mr, rx) << std::endl; 
     std::cout << "  matched: \"" << mr.str() << "\"" << std::endl; 
